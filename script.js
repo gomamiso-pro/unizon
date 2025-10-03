7fe9031943f13d7e3affa5c727f624a3f15485eb
ごまみそ <noritomo3.m.m@gmail.com>
2025-10-03 20:28:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Google Apps Script のURL
-const API_URL = "https://script.google.com/macros/s/AKfycbwaChrXugws6UgEoxfcwiJCVaj_G7EKN8OG-e7Nsm0cKqs6QIf-jJbTqK5maz4jz57h/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyOGEQtDnKi_ryd7TzGfFi60bD3TNRQJOFYKqWaxwEqKaRBLc7ncG6jf2WmlHsHiViN/exec";
 
 // ログイン状態を管理する変数
 let isLoggedIn = false;
