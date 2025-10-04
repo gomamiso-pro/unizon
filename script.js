d49e06c5ea423ad4bdc03d79ebfd0df4a00631af
ごまみそ <noritomo3.m.m@gmail.com>
2025-10-04 14:58:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Google Apps Script のURL (★ こちらのURLを実際のGASのデプロイURLに置き換えてください)
-const API_URL = "https://script.google.com/macros/s/AKfycbw9WlXEjDZqkfGZha6WGCi7oBSRdpziGT4NsgAm1u-7lqEn0OZMd5Do3xIx63u2d91P/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwwrgtFdFXO-Lwi8K4nxYExXntuooRYCFMmRIJhFdCW3CDQVDdusLH3tzjQ8UvLwOKf/exec";
 
 // ログイン状態を管理するための変数
 let isLoggedIn = false;
